b4eaff2c99207d1bd7f4fe16857edf4c244f250a
Leopold Götsch <l.goetsch@reibungsfrei.eu>
2024-03-08 10:03:00 +0100

    Built site for gh-pages

--- symbols.py
+++
@@ -1,6 +0,0 @@
-def define_symbols():
-    sym_legend = {}
-    #Complex symbols
-    Uhcl,Urcl = symbols(r'\underline{U}_h(l),\underline{U}_r(l)',real=false)
-    sym_legend = assign_legend_entry(sym_legend,Uhcl,'Komplexe hinlaufende Spannungswelle')
-    sym_legend = sym_legendassign_legend_entry(sym_legend,Urcl,'Komplexe rücklaufende Spannungswelle')
